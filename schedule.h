6209acaea0bb92f8a2d884b9cfe50f3ffa76d328
Kazuha-Li <cloud2000s@gmail.com>
2019-04-30 22:34:10 +0800

    Update schedule.h

--- schedule.h
+++ schedule.h
@@ -18,6 +18,16 @@
 /* TODO */
 // int PSJF_scheduling(...);
 
+#define FIFO	1
+#define RR	2
+#define SJF	3
+#define PSJF	4
+
+/* Return next process to run */
+//int next_process(struct process *proc, int nproc, int policy);
+
+/* Running scheduler */
+int scheduling(struct process *proc, int nproc, int policy);
 
 
 
